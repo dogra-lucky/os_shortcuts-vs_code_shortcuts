5e09d82114acf659cc8222ea20d625944f00f8dc
lucky <lucky@gmail.com>
2023-03-30 14:30:38 +0530

    vs code shortcuts

---
+++ vsCodeShortcuts.js
@@ -0,0 +1,7 @@
+// to move lines up and down    alt + up_arrow_key/down_arrow_key
+// to add block comments       Ctrl +Shift + A
+// to find next match of selected expression   Ctrl+ d
+// to find file or go to file       Ctrl + P
+// to close current page/tab     Ctrl + w
+// move to next tab/page       ctrl + tab
+// fold/unfold blocks              ctrl + shift + [/]
