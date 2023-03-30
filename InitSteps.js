eff2f31f0f57b4ece2212129f1cb816dc4e78ea5
lucky <lucky@gmail.com>
2023-03-30 11:13:51 +0530

    initial commit

---
+++ InitSteps.js
@@ -0,0 +1,5 @@
+// steps to setup production ready server
+
+//1 >> create account on aws or digital-ocean
+//1.1 >> on digital-ocean create droplet of CentOs
+//1.1 >> on aws create EC2 instance of CentOs
