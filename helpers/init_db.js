6f853a5783ba0ed618b4d87109284bb19fc9f6e1
lucky <dogralucky978@gmail.com>
2023-04-17 09:50:16 +0530

    use  in mongo aggregation

--- helpers/init_db.js
+++ helpers/init_db.js
@@ -13,11 +13,22 @@ client
 
 const db = client.db("mongo_client");
 const clOne = db.collection("clOne");
-clOne
-  .insertOne({ class: "5th", roll_no: "ght" })
-  .then(() => {
-    console.log("data inserted");
-  })
-  .catch((err) => {
-    throw err;
-  });
+// clOne
+//   .insertOne({ class: "5th", roll_no: "ght" })
+//   .then(() => {
+//     console.log("data inserted");
+//   })
+//   .catch((err) => {
+//     throw err;
+//   });`
+// clOne
+//   .aggregate()
+//   .match({ roll_no: 32 })
+//   .out({ coll: "roll_32", db: "mongo_client" });
+async function foo() {
+  const aggIns = clOne.aggregate([
+    { $match: { roll_no: 32 } },
+    { $out: "roll_no_32" },
+  ]);
+}
+foo();
